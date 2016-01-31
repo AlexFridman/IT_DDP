dc9431009e7b3ea184025c9bd157029da1acb163
Alex Fridman <AlexFridman@outlook.com>
2016-01-31 20:54:45 +0300

    Add task 12

---
+++ task 12/task 2/AjaxDemo/AjaxDemo/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AjaxDemo.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<object> Get()
+        {
+            return new List<object>
+            {
+                new {Name = "alex", Age = 19},
+                new {Name = "petia", Age = 49},
+                new {Name = "vasia", Age = 39},
+                new {Name = "vova", Age = 29}
+            };
+        }
+    }
+}
